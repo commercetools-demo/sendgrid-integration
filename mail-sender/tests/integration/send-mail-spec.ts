73a053738dc517fdaf0314d2ad1ac70e8089fac9
Philipp Hofmann <philipp.hofmann@commercetools.com>
2024-11-29 14:23:49 +0100

    Proper data for send email

--- mail-sender/tests/integration/send-mail-spec.ts
+++ mail-sender/tests/integration/send-mail-spec.ts
@@ -2,10 +2,8 @@ import {
   readAdditionalConfiguration,
   readConfiguration,
 } from '../../src/utils/config.utils';
-import request from 'supertest';
-import app from '../../src/app';
-import { expect } from '@jest/globals';
 import { sendMail } from '../../src/handlers/send-mail';
+import { faker } from '@faker-js/faker';
 
 describe('Testing router', () => {
   beforeEach(() => {
@@ -17,7 +15,26 @@ describe('Testing router', () => {
     await sendMail(readAdditionalConfiguration().senderEmailAddress, {
       recipientEmailAddresses: ['philipp.hofmann@commercetools.com'],
       templateId: readAdditionalConfiguration().orderConfirmationTemplateId,
-      templateData: {},
+      templateData: {
+        orderNumber: '2024-11-29-79164',
+        customerEmail: faker.string.sample(),
+        customerFirstName: faker.person.firstName(),
+        customerMiddleName: faker.person.middleName(),
+        customerLastName: faker.person.lastName(),
+        orderCreationTime: '12:50:29',
+        orderCreationDate: '29.11.2024',
+        orderTotalPrice: '1.299,00 €',
+        orderTaxedPrice: '1.091,60 €',
+        orderLineItems: [
+          {
+            productQuantity: 1,
+            productSku: 'NTSS-02',
+            productImage:
+              'https://storage.googleapis.com/merchant-center-europe/sample-data/goodstore/Nala_Two_Seater_Sofa-2.1.jpeg',
+            productSubTotal: '1.299,00 €',
+          },
+        ],
+      },
       successMessage: '',
       preSuccessMessage: '',
     });
