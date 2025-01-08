e4471ea881d45a6c427ab43456fa74c71a853562
Philipp Hofmann <philipp.hofmann@commercetools.com>
2025-01-08 15:30:09 +0100

    not needed anymore

--- denotemplate/templateengine/hbshelpers/imgurl.ts
+++
@@ -1,5 +0,0 @@
-import { Image } from "@commercetoolsdemo/sdk"
-
-export const imgurl = (images: Image[]): string => {
-   return images[0].url
-}
--- denotemplate/templateengine/hbshelpers/resolver.ts
+++
@@ -1,9 +0,0 @@
-export const resolveHBSTemplate = (partialname: string): string => {
-   try {
-     const mjmltemplate = Deno.readTextFileSync(`templateengine/mjml/${partialname.toLowerCase()}.mjml`)
-     return mjmltemplate
-   }
-   catch (_error) {
-     return `could not find partial ${partialname}`
-   }
- }
